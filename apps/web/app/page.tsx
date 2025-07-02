e7a6959d9bbe43d317acbd1e296b1e241c22a45c
Ryan Capio <ryan@ingenious.ae>
2025-07-02 16:10:14 +0400

    add pm2 runtime

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,20 +1,7 @@
-"use client";
-
-import FacebookLogin from "@/components/facebook-login";
 import { Button } from "@workspace/ui/components/button";
 import Link from "next/link";
-import Script from "next/script";
-import { logger } from "@/lib/logger";
 
 export default function Page() {
-  const handleLoginSuccess = (response: any) => {
-    logger.log("Login success", response);
-  };
-
-  const handleLoginFailure = (error: string) => {
-    logger.error("Login failed", error);
-  };
-
   return (
     <div>
       <div className="flex items-center justify-center min-h-svh">
