f00bae1b8964b5c2b57aff484bec7d912fb717cd
Ryan Capio <ryan@ingenious.ae>
2025-07-08 13:00:43 +0400

    updated the message status notification

--- apps/whatsapp-service/src/app/whatsapp/services/webhook/messages/statuses/message-status.ts
+++ apps/whatsapp-service/src/app/whatsapp/services/webhook/messages/statuses/message-status.ts
@@ -1,4 +1,9 @@
+import { ioInstance } from "@/socket";
 import { conversationsTable, conversationStatusEnum, db } from "@workspace/db";
+import {
+  NotificationEvent,
+  NotificationRelatedObject,
+} from "@workspace/shared";
 import WhatsApp, { WebhookMessage } from "@workspace/wa-cloud-api";
 import { eq } from "drizzle-orm";
 
@@ -9,10 +14,38 @@ export async function handleMessageStatus(
   if (!message.statuses) return;
   const { id, status } = message.statuses;
 
-  await db
+  const conversations = await db
     .update(conversationsTable)
     .set({
       status: status,
     })
-    .where(eq(conversationsTable.wamid, id));
+    .where(eq(conversationsTable.wamid, id))
+    .returning();
+
+  const conv = conversations[0];
+
+  if (!conv) return;
+
+  ioInstance
+    .to(`team:${conv.teamId}`)
+    .emit(NotificationEvent.WhatsAppBulkMessageOutgoingSuccess, {
+      payload: {
+        message: "Conversation Status Update",
+      },
+      teamId: conv.teamId,
+      relatedId: conv.id,
+      relatedObject: NotificationRelatedObject.Conversation,
+    });
+
+  if (conv.marketingCampaignId)
+    ioInstance
+      .to(`team:${conv.teamId}`)
+      .emit(NotificationEvent.WhatsAppBulkMessageOutgoingSuccess, {
+        payload: {
+          message: "Conversation Status Update",
+        },
+        teamId: conv.teamId,
+        relatedId: conv.marketingCampaignId,
+        relatedObject: NotificationRelatedObject.MarketingCampaign,
+      });
 }
